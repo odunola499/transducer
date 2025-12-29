429acc48e123ae253f2127b214cd19475f07b47d
odunola <jenrola2292@gmail.com>
2025-12-29 10:20:50 +0100

    FastConformer Encoder

--- scratchpad/nemo_demo.py
+++ scratchpad/nemo_demo.py
@@ -6,21 +6,22 @@ from transducer.models.encoder.fast_conformer import (
     FastConformerConfig,
 )
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(
     model_name="nvidia/parakeet_realtime_eou_120m-v1"
-)
+).to(device)
 
 asr_feature_extractor = asr_model.preprocessor.featurizer
 encoder = asr_model.encoder.eval()
 
-audio = torch.randn(2, 64000)
-lengths = torch.tensor([64000, 64000]).to(torch.long)
+audio = torch.randn(2, 64000, device=device)
+lengths = torch.tensor([64000, 64000], device=device).to(torch.long)
 
 features, lengths = asr_feature_extractor(audio, lengths)
 encoder_outputs, _ = encoder(audio_signal=features, length=lengths)
 
 config = FastConformerConfig()
-conf_encoder = ConformerEncoder(config=config).eval()
+conf_encoder = ConformerEncoder(config=config).eval().to(device)
 conf_encoder.load_state_dict(encoder.state_dict())
 conf_encoder_outputs = conf_encoder(features)
 
